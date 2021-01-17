916c36213efb767f81cfae15f5b73507960f34dd
arunp <arunpr0415@gmail.com>
2021-01-17 12:19:37 +0530

    First Commit

---
+++ src/main/java/com/byob/SpringBootBYOBApplication.java
@@ -0,0 +1,13 @@
+package com.byob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBYOBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBYOBApplication.class, args);
+	}
+
+}
